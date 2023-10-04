4f7dc3f3a85bcce1517904fc6b869d9dfd622310
Aleksander Olsvik <aleksanderolsvik02@gmail.com>
2023-10-04 16:31:13 +0200

    Wups c ikke cpp

---
+++ task5/task5.c
@@ -0,0 +1,13 @@
+//
+// Created by Aleksander Olsvik on 27.09.2023.
+//
+#include "utility.h"
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    printf("%s\n", replace_chars("Hu & hei"));
+
+    printf("%s\n", replace_chars("Hei <3"));
+}
+
