c8f93e47001bf000cefa4ee935c16724942ec9a2
Aleksander Olsvik <aleksanderolsvik02@gmail.com>
2023-10-04 17:12:13 +0200

    360 fuckyou

--- task5/replace_chars.c
+++ task5/replace_chars.c
@@ -1,30 +1,37 @@
-//
-// Created by Aleksander Olsvik on 04.10.2023.
-//
-
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 #include "utility.h"
 
 char* replace_chars(char *s) {
-    // Calculate the size of the input string s
     size_t size_of_s = strlen(s);
-    size_t max_length = size_of_s * (strlen("&amp;")-1) + 1;
-    char *result = (char *) malloc(max_length * sizeof(char));
-    result[0] = '\0';
-    // characters to be replaced: &, <, > with &amp;, &lt;, &gt;
-    // Write rest of function to replace characters and increase length of result
-    for(size_t i = 0; i<size_of_s; i++){
-        if(s[i] == '&'){
-            strcat(result, "&amp;");
-        } else if(s[i] == '<'){
-            strcat(result, "&lt;");
-        } else if(s[i] == '>'){
-            strcat(result, "&gt;");
-        } else {
-            strncat(result, &s[i], 1);
+    size_t max_length = size_of_s; // Start with the original length of the string
+    for (size_t i = 0; i < size_of_s; i++) {
+        if (s[i] == '&' || s[i] == '<' || s[i] == '>') {
+            // Increment the max_length by the difference in length
+            max_length += 4; // For "&amp;", "&lt;", or "&gt;"
         }
     }
+
+    char *result = (char *)malloc((max_length + 1) * sizeof(char));
+    size_t result_idx = 0;
+
+    for (size_t i = 0; i < size_of_s; i++) {
+        if (s[i] == '&') {
+            strcat(result, "&amp;");
+            result_idx += 5;
+        } else if (s[i] == '<') {
+            strcat(result, "&lt;");
+            result_idx += 4;
+        } else if (s[i] == '>') {
+            strcat(result, "&gt;");
+            result_idx += 4;
+        } else {
+            result[result_idx] = s[i];
+            result[result_idx + 1] = '\0';
+            result_idx++;
+        }
+    }
+
     return result;
 }
