76aa6040aa52b64f51d9548ad904c4134f0fd86f
Aleksander Olsvik <aleksanderolsvik02@gmail.com>
2023-10-04 17:13:16 +0200

    360 fuckyou

--- task5/replace_chars.c
+++ task5/replace_chars.c
@@ -1,37 +1,33 @@
-#include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 #include "utility.h"
 
-char* replace_chars(char *s) {
-    size_t size_of_s = strlen(s);
-    size_t max_length = size_of_s; // Start with the original length of the string
-    for (size_t i = 0; i < size_of_s; i++) {
-        if (s[i] == '&' || s[i] == '<' || s[i] == '>') {
-            // Increment the max_length by the difference in length
-            max_length += 4; // For "&amp;", "&lt;", or "&gt;"
-        }
+char* replace_chars(char *in) {
+    size_t l = strlen(in);
+    size_t len = l;
+    for (size_t i = 0; i < l; ++i) {
+        if (in[i] == '&') len += 4;
+        else if (in[i] == '<' || in[i] == '>') len += 3;
     }
+    char *out = malloc(len + 1);
+    if (out == NULL) return NULL;
 
-    char *result = (char *)malloc((max_length + 1) * sizeof(char));
-    size_t result_idx = 0;
-
-    for (size_t i = 0; i < size_of_s; i++) {
-        if (s[i] == '&') {
-            strcat(result, "&amp;");
-            result_idx += 5;
-        } else if (s[i] == '<') {
-            strcat(result, "&lt;");
-            result_idx += 4;
-        } else if (s[i] == '>') {
-            strcat(result, "&gt;");
-            result_idx += 4;
+    size_t pos = 0;
+    for (size_t i = 0; i < strlen(in); i++) {
+        if (in[i] == '&') {
+            strcpy(out + pos, "&amp;");
+            pos += 5;
+        } else if (in[i] == '<') {
+            strcpy(out + pos, "&lt;");
+            pos += 4;
+        } else if(in[i] == '>') {
+            strcpy(out + pos, "&gt;");
+            pos += 4;
         } else {
-            result[result_idx] = s[i];
-            result[result_idx + 1] = '\0';
-            result_idx++;
+            out[pos++]=in[i];
         }
     }
 
-    return result;
+    out[pos] = '\0';
+    return out;
 }
