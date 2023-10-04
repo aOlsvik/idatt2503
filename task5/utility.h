a8541ee40d71edc9fab119e1da6c8d778ece46dd
Aleksander Olsvik <aleksanderolsvik02@gmail.com>
2023-10-04 15:57:50 +0200

    changed function

--- task5/utility.h
+++ task5/utility.h
@@ -4,23 +4,34 @@
 
 #ifndef TASK5_UTILITY_H
 #define TASK5_UTILITY_H
-#include <string>
-using namespace std;
 
-string replace_chars(const string &s) {
-    string new_string;
-    for (char ch : s) {
-        if (ch == '&') {
-            new_string += "&amp";
-        } else if (ch == '<') {
-            new_string += "&lt";
-        } else if (ch == '>') {
-            new_string += "&gt";
+//
+// Created by Aleksander Olsvik on 27.09.2023.
+//
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+char* replace_chars(const char s[]) {
+    // Calculate the size of the input string s
+    size_t size_of_s = strlen(s);
+    size_t max_length = size_of_s * (strlen("&amp;")-1) + 1;
+    char *result = static_cast<char *>(malloc(max_length * sizeof(char)));
+    result[0] = '\0';
+    // characters to be replaced: &, <, > with &amp;, &lt;, &gt;
+    // Write rest of function to replace characters and increase lenght of result
+    for(int i = 0; i<size_of_s; i++){
+        if(s[i] == '&'){
+            strcat(result, "&amp;");
+        } else if(s[i] == '<'){
+            strcat(result, "&lt;");
+        } else if(s[i] == '>'){
+            strcat(result, "&gt;");
         } else {
-            new_string += ch;
+            strncat(result, &s[i], 1);
         }
     }
-    return new_string;
+    return result;
 }
 
 #endif //TASK5_UTILITY_H
