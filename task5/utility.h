45053ef2576d822bdbb2ca29060de43e3a1d8b37
Aleksander Olsvik <aleksanderolsvik02@gmail.com>
2023-10-04 16:04:20 +0200

    Type fix

--- task5/utility.h
+++ task5/utility.h
@@ -16,10 +16,10 @@ char* replace_chars(const char s[]) {
     // Calculate the size of the input string s
     size_t size_of_s = strlen(s);
     size_t max_length = size_of_s * (strlen("&amp;")-1) + 1;
-    char *result = static_cast<char *>(malloc(max_length * sizeof(char)));
+    char *result = (char *) malloc(max_length * sizeof(char));
     result[0] = '\0';
     // characters to be replaced: &, <, > with &amp;, &lt;, &gt;
-    // Write rest of function to replace characters and increase lenght of result
+    // Write rest of function to replace characters and increase length of result
     for(int i = 0; i<size_of_s; i++){
         if(s[i] == '&'){
             strcat(result, "&amp;");
