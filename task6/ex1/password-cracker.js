84bc9abf8f9ab2c625ec45032150f95c3f00de0b
Aleksander Olsvik <aleksanderolsvik02@gmail.com>
2023-10-10 19:23:39 +0200

    finished task2

--- task6/ex1/password-cracker.js
+++ task6/ex1/password-cracker.js
@@ -6,42 +6,22 @@ function passcrack(){
   const iterations = 2048
   const key = "ab29d7b5c589e18b52261ecba1d3a7e7cbf212c6"
   const keylen = key.length / 2
-  const digest = 'sha512'
+  const digest = 'sha1'
 
-  for(let i = 0; i<36; i++){
-    for(let j = 0; j<36; j++){
-        for(let k=0; k<36; k++){
-          for(let l=0; l<36; l++){
-            password = ''
-            if(i<26){
-              password += String.fromCharCode(97+i)
-            }
-            else{
-              password += (i-26).toString()
-            }
-            if(j<26){
-              password += String.fromCharCode(97+j)
-            }
-            else{
-              password += (j-26).toString()
-            }
-            if(k<26){
-              password += String.fromCharCode(97+k)
-            }
-            else{
-              password += (k-26).toString()
-            }
-            if(l<26){
-              password += String.fromCharCode(97+l)
-            }
-            else{
-              password += (l-26).toString()
-            }
-            console.log("Password: " + password)
-            const hash = crypto.pbkdf2Sync(password, salt, iterations, keylen, digest).toString('hex')
-            if(hash === key){
-              console.log("Password: " + password)
-              return
+  //nothing found up until i = 2, set start = 3
+
+  for(let i = 3; i<26; i++){
+    console.log(i)
+    for(let j = 0; j<26; j++){
+        for(let k=0; k<26; k++){
+          for(let l=0; l<26; l++){
+            for(let m=0; m<26; m++){
+              password = String.fromCharCode(97+i, 97+j, 97+k, 97+l, 97+m)
+              const hash = crypto.pbkdf2Sync(password, salt, iterations, keylen, digest).toString('hex')
+              if(hash == key){
+                console.log("Password found: " + password)
+                return
+              }
             }
           }
         }
